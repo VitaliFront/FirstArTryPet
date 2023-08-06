1b97c23379944262d2d9e6c3db985c1f4cc4b4cc
Vitali Klopau <vitaliklopau@gmail.com>
2023-08-06 23:51:57 +0200

    Initial Commit

---
+++ guitar/guitarApp.swift
@@ -0,0 +1,17 @@
+//
+//  guitarApp.swift
+//  guitar
+//
+//  Created by Vitali Klopau on 06.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct guitarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
